8f713d2f0ee71727c5c475f0c60938bc0369fe93
RiskiVR <40964931+RiskiVR@users.noreply.github.com>
2021-11-14 13:21:27 -0800

    Update Source (v2.0)
    See changelog for v2.0

---
+++ Assets/RiskiPlayer/Scripts/RiskiPlayerUDON.cs
@@ -0,0 +1,77 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RiskiPlayerUDON : UdonSharpBehaviour
+{
+    [Header("RiskiPlayer Settings")]
+    public bool MenuOnStart;
+
+    [Header("Internal Elements")]
+    public AudioSource ClickSound;
+    public Animator Main;
+    public GameObject EnterUIButton;
+    public GameObject ExitUIButton;
+    public GameObject FullscreenButton;
+    public GameObject DesktopScreen;
+    public Slider BrightnessSlider;
+    public MeshRenderer Brightness;
+    public Animator URLButton;
+    public Button URL;
+
+    private void Start()
+    {
+        if (MenuOnStart == false)
+        {
+            Main.SetBool("MenuActive", false);
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            DesktopScreen.SetActive(false);
+        }
+    }
+    public void BootUI()
+    {
+        Main.SetBool("MenuActive", true);
+        URL.interactable = true;
+        ExitUIButton.SetActive(true);
+        EnterUIButton.SetActive(false);
+        Main.SetTrigger("Fade");
+        Debug.Log("UI has been booted");
+        if (Networking.LocalPlayer.IsUserInVR() == false)
+            FullscreenButton.SetActive(true);
+    }
+    public void ExitUI()
+    {
+        Main.SetBool("MenuActive", false);
+        Main.SetTrigger("Reset");
+        ExitUIButton.SetActive(false);
+        EnterUIButton.SetActive(true);
+        Debug.Log("UI has been closed");
+    }
+    public void OnBrightnessValueChanged()
+    {
+        var color = Brightness.materials[0].GetColor("_Color");
+        color.a = 1 - BrightnessSlider.value;
+        Brightness.materials[0].SetColor("_Color", color);
+    }
+    public void Fullscreen()
+    {
+        if (Networking.LocalPlayer.IsUserInVR() == false)
+        {
+            DesktopScreen.SetActive(true);
+            ClickSound.Play();
+        }   
+    }
+    public void EnterURL()
+    {
+        Main.SetTrigger("Click");
+        URL.interactable = false;
+    }
+}
